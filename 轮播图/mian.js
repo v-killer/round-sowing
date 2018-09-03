f0a11c4a3a6657fa8724db6e5d36b53203c5a82f
shen <343206898@qq.com>
2018-09-03 19:07:15 +0800

    1

---
+++ 轮播图/mian.js
@@ -0,0 +1,20 @@
+$(p1).on('click',function(){
+    $(images).css({
+        transform: 'translateX(0)'
+    })
+})
+$(p2).on('click',function(){
+    $(images).css({
+        transform: 'translateX(-500px)'
+    })
+})
+$(p3).on('click',function(){
+    $(images).css({
+        transform: 'translateX(-1000px)'
+    })
+})
+$(p4).on('click',function(){
+    $(images).css({
+        transform: 'translateX(-1500px)'
+    })
+})
